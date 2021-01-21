b35539ec56cc308e1c45b5e7bf8ea3f98628f2cd
John <john.teroso@hotmail.fr>
2021-01-21 21:06:50 +0100

    f

--- lib/mairie.rb
+++ lib/mairie.rb
@@ -1,4 +1,4 @@
-But de ce programme :
+# But de ce programme
 # Recuperer tout d'abord la liste des url pour les # mairies du val d'oise
 # Recuperer sur chacune de ces pages l'adresse mail pour les joindre
 
